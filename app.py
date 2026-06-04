447c1cd994458100dec61ab00bfd62948e3ede65
riyandana07 <riyandana060704@gmail.com>
2026-06-04 21:47:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ with st.sidebar:
     col1, col2, col3 = st.columns([1,3,1])
 
     with col2:
-        st.image("logo uniska.png", width=150)
+        st.image("logo uniska.png", width=120)
 
     st.title("🌿 Sistem Identifikasi TOGA")
 
