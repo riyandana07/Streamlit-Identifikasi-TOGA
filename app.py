23f38d64a5eea30a2cbcdf67a74fb08c98177efd
riyandana07 <riyandana060704@gmail.com>
2026-06-19 07:44:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ section[data-testid="stSidebar"] * {
 # =========================
 # LOAD MODEL
 # =========================
-model = YOLO("best (2).pt")
+model = YOLO("best (1).pt")
 
 # =========================
 # INFORMASI TANAMAN
